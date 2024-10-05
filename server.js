49c2501ca2103b9b41bfd2fcc4e74e551259ee81
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-10-05 19:08:30 +0530

    new

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://ecub-bot3.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // Initialize Firebase Admin SDK
