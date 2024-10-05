21aa7711d02b9304663dbc773b40c3a29a460f0a
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-10-05 18:52:56 +0530

    api

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ const db = admin.firestore();
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Define User Schema
 const UserSchema = new mongoose.Schema({
